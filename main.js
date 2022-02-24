eaeded3e156448a6f7d3d59b2a52baebf0a9e616
Wade Edwards <wadecedwards@gmail.com>
2022-02-23 18:30:31 -0600

    Add files via upload
    Added Lightbox functionality to photos on home page.

--- main.js
+++ main.js
@@ -41,3 +41,25 @@ next.addEventListener('click', e => {
   moveSlider(slider, currentImg, nextImg);
 });
 
+// lightbox 
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+const lightboxImages = document.querySelectorAll('.gallery-slide img');
+
+lightboxImages.forEach(i => {
+  i.addEventListener('click', () => {
+    lightbox.classList.add('active');
+    const lightboxImg = document.createElement('img');
+    lightboxImg.src = i.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(lightboxImg); 
+  })
+})
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+})
