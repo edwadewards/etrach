79534932323ac635d40d067abd4a1084132b5978
Wade Edwards <wadecedwards@gmail.com>
2022-05-28 17:42:37 -0500

    Add files via upload
    Small design tweaks.

--- pages/events.js
+++ pages/events.js
@@ -1,3 +1,33 @@
+// Intersection Observers
+const fadeIn = document.querySelectorAll('.photo-gallery > img');
+
+const observerOptions = {
+  threshold: .6,
+  rootMargin: "0px 0px -40px 0px"
+};
+
+const scrollsIn = new IntersectionObserver
+(function(
+  entries, 
+  scrollsIn
+) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      scrollsIn.unobserve(entry.target);
+    }
+  });
+},
+observerOptions);
+
+fadeIn.forEach(element => {
+  scrollsIn.observe(element);
+});
+
+
+
 // lightbox 
 const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
